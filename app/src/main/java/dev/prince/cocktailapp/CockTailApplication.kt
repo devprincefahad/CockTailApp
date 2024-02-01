bba51834471d2a0efb25d2f4e18bf4558351b640
devprincefahad <1pfchouhan@gmail.com>
2024-02-01 19:25:17 +0530

    Hilt DI integration

---
+++ app/src/main/java/dev/prince/cocktailapp/CockTailApplication.kt
@@ -0,0 +1,5 @@
+package dev.prince.cocktailapp
+
+import android.app.Application
+
+class CockTailApplication: Application()
---
+++ app/src/main/java/dev/prince/cocktailapp/di/AppModule.kt
@@ -0,0 +1,16 @@
+package dev.prince.cocktailapp.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+}
